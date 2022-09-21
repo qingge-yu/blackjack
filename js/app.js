4fd897580ef75509e8fc699e9a48f955372747aa
qingge-yu <maggieqgg@gmail.com>
2022-09-20 20:05:30 -0700

    finishes drawCard() and addCardToHand()

--- js/app.js
+++ js/app.js
@@ -1,48 +1,40 @@
 /*----- constants -----*/
 
 const suits = ['d', 'h', 's', 'c']
-
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A']
-
-let deck = []
-
-shuffle()
+let deck = null
+newDeck()
 
 const PLAYER_LOOKUP = {
     '1': {
         name: 'Player',
     },
     '-1': {
-        name: 'House',
+        name: 'Dealer',
     }
 }
 
 /*----- app's state (variables) -----*/
 
-let turn, cardValue, cardValueTotal, winner
+let turn, cardValue, playerCardValueTotal, dealerCardValueTotal, winner
 
-let card = deck[Math.floor(Math.random() * 52)]
-
-
-
+const playerHand = []
+const dealerHand = []
+const playedCards = []
+let cardCount = 0
 
 
 /*----- cached element references -----*/
 
 const resetBtnEl = document.getElementById('reset-btn')
-
 const hitBtnEl = document.getElementById('hit-btn')
-
 const standBtnEl = document.getElementById('stand-btn')
-
-const houseCardsEl = document.querySelector('.house-cards')
-
 const playerCardsEl = document.querySelector('.player-cards')
-
+const dealerCardsEl = document.querySelector('.house-cards')
 const messageEl = document.querySelector('h2')
 
 
-// /*----- event listeners -----*/
+/*----- event listeners -----*/
 
 resetBtnEl.addEventListener('click', handleResetClick)
 
@@ -51,82 +43,9 @@ hitBtnEl.addEventListener('click', handleHitClick)
 standBtnEl.addEventListener('click', handleStandClick)
 
 
-// /*----- functions -----*/
+/*----- functions -----*/
 
-function init() {
-    shuffle()
-    turn = 1
-    winner = null
-    playerCardCount = 0
-    houseCardCount = 0
-    cardValueTotal = 0
-}
-
-function handleResetClick() {
-    init()
-
-    console.log(deck)
-
-}
-
-function handleStandClick() {
-    changeTurn()
-    houseTurn()
-    console.log(deck)
-}
-
-function changeTurn() {
-    turn *= -1
-}
-
-function handleHitClick() {
-    drawCard()
-
-    console.log(deck)
-
-}
-
-function houseTurn() {
-    drawCard()
-    houseAddCardToHand()
-}
-
-function drawCard() {
-    let idx = 1
-    deck = deck.filter(function (usedCard) {
-        return usedCard !== card
-    })
-    card = deck[Math.floor(Math.random() * (52 - idx))]
-    idx++
-    playerAddCardToHand()
-}
-
-function playerAddCardToHand() {
-    const playerNewCard = document.createElement('div')
-    playerNewCard.classList.add('card')
-    playerNewCard.classList.add(`${card}`)
-    playerCardsEl.appendChild(playerNewCard)
-}
-
-function houseAddCardToHand() {
-    const houseNewCard = document.createElement('div')
-    houseNewCard.classList.add('card')
-    houseNewCard.classList.add('back')
-    houseCardsEl.appendChild(houseNewCard)
-}
-
-function handValue() {
-    cardValueTotal += `${card}`
-    if (card[1] === 'J' || card[1] === 'Q' || card[1] === 'K' || card[1] === '1') {
-        cardValue = 10
-    } else if (card[1] === 'A') {
-        cardValue = 1
-    } else {
-        cardValue = card[2]
-    }
-}
-
-function shuffle() {
+function newDeck() {
     deck = []
     for (let suitIdx = 0; suitIdx < 4; suitIdx++) {
         for (let rankIdx = 0; rankIdx < 13; rankIdx++) {
@@ -135,12 +54,104 @@ function shuffle() {
     }
 }
 
-// function checkWin() {
+function init() {
+    newDeck()
+    turn = 1
+    winner = null
+    playerCardValueTotal = 0
+    dealerCardValueTotal = 0
+}
+
+function handleResetClick() {
+    init()
+    console.log(deck)
+}
+
+function handleHitClick() {
+    playerTurn()
+    console.log(deck)
+}
+
+function handleStandClick() {
+    changeTurn()
+    dealerTurn()
+    console.log(deck)
+}
+
+function changeTurn() {
+    turn *= -1
+}
+
+function dealerTurn() {
+    drawCard(dealerHand)
+
+}
+
+function playerTurn() {
+    drawCard(playerHand)
+    console.log("player hand: " + playerHand)
+
+}
+
+function drawCard(array) {
+    let randomCard = deck[Math.floor(Math.random() * (52 - cardCount))]
+    console.log("card: " + randomCard)
+    if (!playedCards.includes(randomCard)) {
+        playedCards.push(randomCard)
+        array.push(randomCard)
+        deck = deck.filter(function (usedCard) {
+            return usedCard !== randomCard
+        })
+        console.log("played cards:" + playedCards)
+        addCardToHand()
+        cardCount++
+    }
+    console.log("turn: " + turn)
+}
+
+
+function addCardToHand() {
+    if (turn == '1') {
+        let playerNewCard = document.createElement('div')
+        playerNewCard.classList.add('card', `${playerHand[playerHand.length - 1]}`)
+        playerCardsEl.appendChild(playerNewCard)
+    } else if (turn == '-1') {
+        let dealerNewCard = document.createElement('div')
+        dealerNewCard.classList.add('card', 'back')
+        dealerCardsEl.appendChild(dealerNewCard)
+    }
+}
+
+function handValue(array) {
+    cardValue = 0
+    array.forEach(function (idx) {
+        let arr = idx.split('')
+        let cardIdx = arr[arr.length - 1]
+        if (cardIdx === 'J' || cardIdx === 'Q' || cardIdx === 'K' || cardIdx === '0') {
+            cardValue = 10
+        } else if (cardIdx === 'A') {
+            cardValue = 1
+        } else {
+            cardValue = cardIdx
+        }
+    })
+    console.log(cardValue)
+}
+
+// function playerHandValueTotal() {
+//     if (playerAddCardToHand) {
+//         playerCardValueTotal += cardValue
+//     }
 // }
 
-// function render() {
-//     if ()
-// }
+
+
+// // function checkWin() {
+// // }
+
+// // function render() {
+// //     if ()
+// // }
 
 
 // start game
